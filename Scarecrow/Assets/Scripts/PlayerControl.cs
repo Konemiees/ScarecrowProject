e5425afe3bd75f88452f0001d60365307139aee6
Konemiees <konsta.sinisalo@gmail.com>
2015-06-09 21:42:17 +0300

    Liikkuva kuutio

--- Scarecrow/Assets/Scripts/PlayerControl.cs
+++ Scarecrow/Assets/Scripts/PlayerControl.cs
@@ -1,15 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(PlayerPhysics))]
+
 public class PlayerControl : MonoBehaviour {
+
+	public float maxSpeed = 1;
+	public float acceleration = 8;
+	public float turnSpeed = 10;
+	public float turnAcceleration = 10;
+
+	private float currentSpeed;
+	private float currentTurn;
+
+	private PlayerPhysics playerPhysics;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		currentSpeed = 0;
+		playerPhysics = GetComponent<PlayerPhysics> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		var coordX = Input.GetAxis ("Horizontal");
+		var coordZ = Input.GetAxis ("Vertical");
+
+		currentTurn = IncrementTowards (currentTurn, turnSpeed * Time.deltaTime * coordX, turnAcceleration);
+		currentSpeed = IncrementTowards (currentSpeed, maxSpeed * Time.deltaTime * coordZ, acceleration);
+
+
+		playerPhysics.move (new Vector3(currentTurn, 0, currentSpeed));
+
 	}
+
+	private float IncrementTowards(float n, float target, float a){
+		if(n == target){
+			return n;
+		}
+		else{
+			float dir = Mathf.Sign(target-n);
+			n+=a*Time.deltaTime*dir;
+			return(dir == Mathf.Sign(target-n))? n: target;
+		}
+	}
+
 }
--- Scarecrow/Assets/Scripts/PlayerPhysics.cs
+++ Scarecrow/Assets/Scripts/PlayerPhysics.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class PlayerPhysics : MonoBehaviour {
 
+	public float gravity = 0;
+
+
+
+
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void move (Vector3 movement) {
+
+
+		transform.Translate (new Vector3(0, 0, movement.z));
+		transform.Rotate(new Vector3(0, movement.x, 0));
+
 	}
+
+
+
+
 }
