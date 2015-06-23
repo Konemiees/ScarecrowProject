98867cc43fc2e4d34d740eacd712cac3c7c1d53b
Konemiees <konsta.sinisalo@gmail.com>
2015-06-23 17:44:30 +0300

    Melkee toimiva kamera

--- Scarecrow/Assets/Scripts/CameraFollow.cs
+++ Scarecrow/Assets/Scripts/CameraFollow.cs
@@ -14,11 +14,16 @@ public class CameraFollow : MonoBehaviour
 	private float x = 0.0f; 
 	private float y = 0.0f;
 
+	public float cameraLowLimit = 0;
+	public float cameraUpAngleLimit = 58;
+	public float cameraCloseLimit = 1;
+	private bool cameraFrontLock;
+
 	//@script AddComponentMenu("Camera-Control/Mouse Orbit");
 	
 	void Start ()
 	{
-		 
+		cameraFrontLock = false;
 		x = transform.eulerAngles.y; 
 		y = transform.eulerAngles.x;
 	
@@ -31,23 +36,39 @@ public class CameraFollow : MonoBehaviour
 		// Create a postion the camera is aiming for based on the offset from the target.
 		Vector3 targetCamPos = target.position + offset;
 		
+
 		// Smoothly interpolate between the camera's current position and it's target position.
 		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
 
 		 
-
-		
-
-			
-		
-		
 		 if (target) { 
 			x += Input.GetAxis("Mouse X") * xSpeed * distance; //0.02f; 
 			y -= Input.GetAxis("Mouse Y") * ySpeed; //0.02f; 
+
+
+			if (y > cameraUpAngleLimit){
+				y = cameraUpAngleLimit;
+			}
+
 			Quaternion rotation = Quaternion.Euler(y, x, 0); 
 			Vector3 position = rotation * new Vector3(0.0f, 2.0f, -distance) + target.position; 
-			transform.rotation = rotation; 
+
+			float dist = Mathf.Sqrt(Mathf.Abs(target.position.z-position.z)*Mathf.Abs(target.position.z-position.z)+(Mathf.Sqrt(Mathf.Abs(target.position.x-position.x)*Mathf.Abs(target.position.x-position.x)+Mathf.Abs(target.position.y-position.y)*Mathf.Abs(target.position.y-position.y))));
+
+			print(dist);
+
+			if(dist <= cameraCloseLimit && y > 0){
+				print("täsä");
+				rotation = Quaternion.Euler(0, x, 0);
+				position = rotation * new Vector3(0.0f, 2.0f, -distance) + target.position;
+			}
+
+			if (position.y < cameraLowLimit) {
+				position.y = cameraLowLimit;
+				rotation = Quaternion.Euler(0, x, 0); 
+			} 
 			transform.position = position;
+			transform.rotation = rotation;
 		}
 
 	}
